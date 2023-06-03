507c2146f086673a849f6777c9ae513eb2a8ece7
Andrew Klaus <andrew@aklaus.ca>
2023-06-03 11:30:10 -0600

    Fixing water parsing for april

--- epcor-water.py
+++ epcor-water.py
@@ -1,10 +1,9 @@
 import argparse
-import calendar
 import datetime
 import logging
 import requests
 import sys
-from decimal import *
+from decimal import Decimal, ROUND_UP, InvalidOperation
 from bs4 import BeautifulSoup
 from io import BytesIO
 from pdfminer.high_level import extract_text
@@ -34,6 +33,10 @@ class WaterReport():
     def bicarbonate(self):
         return (self.alkalinity/50)*61
 
+    def get_influxdb(self):
+        # line protocol
+        # measurement,tag=foo value=bar ts
+        pass
 
 def download_daily_data(report, zone):
     url = f"https://apps.epcor.ca/DailyWaterQuality/Default.aspx?zone={zone}"
@@ -50,7 +53,7 @@ def download_daily_data(report, zone):
         report.alkalinity = Decimal(soup.find(id="AlkalinityLabel7").text)
         date = soup.find(id="DateLabel7").text
         print(f"Daily data from: {date}")
-    except decimal.InvalidOperation:
+    except InvalidOperation:
         # Data invalid, fallback to previous day
         try:
             report.ph = Decimal(soup.find(id="phLabel6").text)
@@ -80,6 +83,90 @@ def parse_lines_from_pdf(pdf_file):
   # return PDF lines
   return lines
 
+def parse_parameter_names(pdf_lines):
+    parameters = []
+    parameter_name_start = False
+
+    for line in pdf_lines:
+        logging.debug(f"Parsing line {line} for parameter name")
+
+        if line.startswith('Alkalinity'):
+            logging.debug("Found Alkalinity. Starting parameter name parsing")
+            parameter_name_start = True
+
+        # Bacteriological Data is a heading, and not a parameter
+        if parameter_name_start and line.startswith('Bacteriological Data'):
+            logging.debug("Ignoring Bacteriological Data parameter")
+            continue
+
+        # e. coli is the last parameter name
+        if line.lower().startswith('e. coli'):
+            logging.debug("Found e.coli. Done with Parameter name parsing")
+            parameters.append(line)
+            return parameters
+
+        if parameter_name_start and line.strip() != "":
+            parameters.append(line)
+
+
+def parse_units(pdf_lines, max_count):
+    units = []
+    units_start = False
+
+    for line in pdf_lines:
+        if line.startswith('mg'):
+            units_start = True
+
+        # Add to units if not finished with them yet
+        if units_start and line.strip() != "":
+            units.append(line)
+
+        if len(units) == max_count:
+            return units
+
+def parse_monthly_counts(pdf_lines, max_counts):
+    counts = []
+
+    for line in pdf_lines:
+        if len(counts) == max_counts:
+            return counts
+
+        # Monthly counts must always be integers
+        try:
+            counts.append(int(line))
+        except ValueError:
+            continue
+
+def parse_monthly_averages(pdf_lines, max_counts):
+    # Once we hit our first set of integers, skip that many
+    # These are monthly counts
+
+    averages = []
+
+    start_integers = False
+    skip_values = 0
+    for line in pdf_lines:
+        if not start_integers:
+            try:
+                int(line)
+                start_integers = True
+            except ValueError:
+                continue
+
+        if skip_values <= max_counts:
+            skip_values += 1
+            continue
+
+        # Ignore any empty rows. Assuming "Alkalinity" row always has a value
+        if len(averages) <= 0 and line.strip() == "":
+            continue
+
+        averages.append(line)
+
+        # -2 to ignore Bacteriological Data
+        if len(averages) == max_counts-2:
+            return averages
+
 def parse_values(pdf_lines, report, print_report=False):
     headers = []
     units = []
@@ -102,79 +189,33 @@ def parse_values(pdf_lines, report, print_report=False):
     ytd_median_start = False
     ytd_median_done = False
 
-    skip_count = 0
+    data = {}
 
-    for line in pdf_lines:
-        # Ignore empty lines until we start ingesting values 
-        # We need to include some empty values after we start monthly average values
-        if line.strip() == "" and not monthly_average_start:
-            continue
+    p = parse_parameter_names(pdf_lines)
+    data['parameters'] = p
+    # TODO: Fix. Remove "headers" name, since this is not an accurate name
+    data['headers'] = p
 
-        # Loop until we get to first header
-        if line.startswith('Alkalinity'):
-            header_start = True
 
-        if line.startswith('Bacteriological Data'):
-            continue
+    logging.debug(f"Got parameter names: {p}")
 
-        # Done with headers
-        if line.lower().startswith('e. coli'):
-            headers.append(line)
-            header_done = True
-            continue
+    u = parse_units(pdf_lines, len(p))
+    data['units'] = u
+    logging.debug(f"Got unit types: {u}")
 
-        # We're into the headers
-        if header_start and not header_done:
-            headers.append(line)
+    # Use unit_count length next, since it's more accurate
+    mc = parse_monthly_counts(pdf_lines, len(u))
+    logging.debug(f"Got monthly counts: {mc}")
 
-        # Start units column
-        if header_done and not units_start:
-            if line.startswith('mg'):
-                units_start = True
-
-        # Units column is done
-        if header_start and len(units) == len(headers):
-            units_done = True
-
-        # Add to units if not finished with them yet
-        if units_start and not units_done:
-            units.append(line)
-
-        # Start monthly count column
-        if units_done and not monthly_count_start:
-            monthly_count_start = True
-
-        if header_done and len(monthly_count) == len(headers):
-            monthly_count_done = True
-
-        if monthly_count_start and not monthly_count_done:
-            monthly_count.append(line)
-
-        # Start monthly average column
-        if monthly_count_done and not monthly_average_start:
-            monthly_average_start = True
-
-        # Subtract 2 from headers list since they include Bacteriological data which messes with things
-        if header_done and len(monthly_average) == len(headers)-2:
-            monthly_average_done = True
-
-        if monthly_average_start and not monthly_average_done:
-            monthly_average.append(line)
-
-        if monthly_average_done and not ytd_median_start and line != "":
-            ytd_median_start = True
-
-        if ytd_median_start and not ytd_median_done:
-            ytd_median.append(line)
+    ma = parse_monthly_averages(pdf_lines, len(u))
+    data['monthly_average'] = ma
+    logging.debug(f"Got monthly averages: {ma}")
 
     if print_report:
-        for i in range(len(headers)-2):
-            print("{}{}{}".format(headers[i].ljust(30), units[i].ljust(12), monthly_average[i].rjust(10) ))
+        for i in range(len(p)-2):
+            print("{}{}{}".format(p[i].ljust(30), u[i].ljust(12), ma[i].rjust(10) ))
 
-    data = {'headers': headers,
-            'units': units,
-            'monthly_average': monthly_average,
-            }
+    logging.debug(f"Parsed data: {data}")
 
     return data
 
@@ -217,7 +258,11 @@ def main():
     parser = argparse.ArgumentParser(description='Calculate water stats from EPCOR water reports')
     parser.add_argument('--zone','-z', choices=['ELS', 'Rossdale'], default='ELS')
     parser.add_argument('--full', action="store_true", default=False)
+    parser.add_argument('--debug', action="store_true", default=False)
     args = parser.parse_args()
+
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG)
 
     report = WaterReport()
     download_daily_data(report, zone=args.zone)
